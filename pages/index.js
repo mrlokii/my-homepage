d1f40dad893ac20bb17ad9bb7857884e80530fb2
mrlokii <107125402+mrlokii@users.noreply.github.com>
2022-06-22 19:28:34 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -104,123 +104,123 @@ const Home = () => (
         </Box>
       </Section>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1984</BioYear>
-          Born in Osaka (大阪), Japan.
-        </BioSection>
-        <BioSection>
-          <BioYear>2010</BioYear>
-          Completed the Master&apos;s Program in the Graduate School of
-          Information Science at Nara Institute of Science and Technology
-          (奈良先端科学技術大学院大学情報科学研究科修士課程)
-        </BioSection>
-        <BioSection>
-          <BioYear>2010</BioYear>
-          Worked at Yahoo! Japan (ヤフー株式会社入社)
-        </BioSection>
-        <BioSection>
-          <BioYear>2012 to present</BioYear>
-          Working as a freelancer
-        </BioSection>
-      </Section>
+//       <Section delay={0.2}>
+//         <Heading as="h3" variant="section-title">
+//           Bio
+//         </Heading>
+//         <BioSection>
+//           <BioYear>1984</BioYear>
+//           Born in Osaka (大阪), Japan.
+//         </BioSection>
+//         <BioSection>
+//           <BioYear>2010</BioYear>
+//           Completed the Master&apos;s Program in the Graduate School of
+//           Information Science at Nara Institute of Science and Technology
+//           (奈良先端科学技術大学院大学情報科学研究科修士課程)
+//         </BioSection>
+//         <BioSection>
+//           <BioYear>2010</BioYear>
+//           Worked at Yahoo! Japan (ヤフー株式会社入社)
+//         </BioSection>
+//         <BioSection>
+//           <BioYear>2012 to present</BioYear>
+//           Working as a freelancer
+//         </BioSection>
+//       </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Art, Music,{' '}
-          <Link href="https://illust.odoruinu.net/" target="_blank">
-            Drawing
-          </Link>
-          , Playing Drums,{' '}
-          <Link href="https://500px.com/p/craftzdog" target="_blank">
-            Photography
-          </Link>
-          , Leica, Machine Learning
-        </Paragraph>
-      </Section>
+//       <Section delay={0.3}>
+//         <Heading as="h3" variant="section-title">
+//           I ♥
+//         </Heading>
+//         <Paragraph>
+//           Art, Music,{' '}
+//           <Link href="https://illust.odoruinu.net/" target="_blank">
+//             Drawing
+//           </Link>
+//           , Playing Drums,{' '}
+//           <Link href="https://500px.com/p/craftzdog" target="_blank">
+//             Photography
+//           </Link>
+//           , Leica, Machine Learning
+//         </Paragraph>
+//       </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          On the web
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/inkdrop_app" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @inkdrop_app (English)
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @craftzdog (日本語)
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://instagram.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
+//       <Section delay={0.3}>
+//         <Heading as="h3" variant="section-title">
+//           On the web
+//         </Heading>
+//         <List>
+//           <ListItem>
+//             <Link href="https://github.com/craftzdog" target="_blank">
+//               <Button
+//                 variant="ghost"
+//                 colorScheme="teal"
+//                 leftIcon={<IoLogoGithub />}
+//               >
+//                 @craftzdog
+//               </Button>
+//             </Link>
+//           </ListItem>
+//           <ListItem>
+//             <Link href="https://twitter.com/inkdrop_app" target="_blank">
+//               <Button
+//                 variant="ghost"
+//                 colorScheme="teal"
+//                 leftIcon={<IoLogoTwitter />}
+//               >
+//                 @inkdrop_app (English)
+//               </Button>
+//             </Link>
+//           </ListItem>
+//           <ListItem>
+//             <Link href="https://twitter.com/craftzdog" target="_blank">
+//               <Button
+//                 variant="ghost"
+//                 colorScheme="teal"
+//                 leftIcon={<IoLogoTwitter />}
+//               >
+//                 @craftzdog (日本語)
+//               </Button>
+//             </Link>
+//           </ListItem>
+//           <ListItem>
+//             <Link href="https://instagram.com/craftzdog" target="_blank">
+//               <Button
+//                 variant="ghost"
+//                 colorScheme="teal"
+//                 leftIcon={<IoLogoInstagram />}
+//               >
+//                 @craftzdog
+//               </Button>
+//             </Link>
+//           </ListItem>
+//         </List>
 
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            href="https://www.youtube.com/devaslife"
-            title="Dev as Life"
-            thumbnail={thumbYouTube}
-          >
-            My YouTube channel (&gt;100k subs)
-          </GridItem>
-          <GridItem
-            href="https://www.inkdrop.app/"
-            title="Inkdrop"
-            thumbnail={thumbInkdrop}
-          >
-            A Markdown note-taking app
-          </GridItem>
-        </SimpleGrid>
+//         <SimpleGrid columns={[1, 2, 2]} gap={6}>
+//           <GridItem
+//             href="https://www.youtube.com/devaslife"
+//             title="Dev as Life"
+//             thumbnail={thumbYouTube}
+//           >
+//             My YouTube channel (&gt;100k subs)
+//           </GridItem>
+//           <GridItem
+//             href="https://www.inkdrop.app/"
+//             title="Inkdrop"
+//             thumbnail={thumbInkdrop}
+//           >
+//             A Markdown note-taking app
+//           </GridItem>
+//         </SimpleGrid>
 
-        <Box align="center" my={4}>
-          <NextLink href="/posts" passHref scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              Popular posts
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
+//         <Box align="center" my={4}>
+//           <NextLink href="/posts" passHref scroll={false}>
+//             <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+//               Popular posts
+//             </Button>
+//           </NextLink>
+//         </Box>
+//       </Section>
     </Container>
   </Layout>
 )
